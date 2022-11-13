0c85583c9f4716b795e34e204692e972c6575f69
Zihan Chen <me@zihan.ga>
2022-11-13 17:50:00 +0800

    Update

--- lib/getToken.ts
+++ lib/getToken.ts
@@ -1,11 +1,9 @@
 import { getToken as getT } from "next-auth/jwt"
 
-export const getToken = (args: any) => (
-    process.env.NODE_ENV === 'production' ? getT(args) : (
-        {
-            name: 'zihan',
-            email: 'me@zihan.ga',
-            
-        }
-    )
-)
+export const getToken = (args: any) =>
+	process.env.NODE_ENV === "production"
+		? getT(args)
+		: {
+				name: "zihan",
+				email: "me@zihan.ga",
+		  }
--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -3,7 +3,11 @@ import { unstable_getServerSession } from "next-auth"
 import { authOptions } from "./auth/[...nextauth]"
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-    const session = await unstable_getServerSession(req as any, res, authOptions)
+	const session = await unstable_getServerSession(
+		req as any,
+		res,
+		authOptions
+	)
 
-    res.end(JSON.stringify(session))
+	res.end(JSON.stringify(session))
 }
